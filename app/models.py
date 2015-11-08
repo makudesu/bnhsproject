3a1fea0dbb8d2c524f25774358388ad4b0a943d9
makudesu <bnhsclouden@gmail.com>
2015-11-08 12:38:50 +0800

    Reverted to ADDED README

--- app/models.py
+++ app/models.py
@@ -1,12 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class MyUser(AbstractUser):
-    first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    sex = models.CharField(max_length=1, choices=SEX_TYPE, null=False)
-    date_of_birth = models.DateField(null=True)
-
-    def __unicode__(self):
-        return self.email
+# Create your models here.
