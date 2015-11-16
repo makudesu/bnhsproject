9de626e33f2e1f713f20b643b2966c6c03226a25
makudesu <bnhsclouden@gmail.com>
2015-11-16 20:45:12 +0800

    Added DetailView with slug

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DetailView
 from django.contrib import messages
 
 from .forms import  CustomUserChangeForm
@@ -8,4 +8,7 @@ from .models import CustomUser
 class CustomUserUpdate(UpdateView):
     form = CustomUserChangeForm
     model = CustomUser
-    fields = ('email', 'first_name', 'middle_name', 'last_name', 'date_of_birth', 'sex',)
+    fields = ('first_name', 'middle_name', 'last_name', 'date_of_birth', 'sex',)
+
+class CustomUserDetail(DetailView):
+    model = CustomUser
